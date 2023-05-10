cf7523303466064df8d36b13bcf8ceacdb588ab2
Jeff-the-simp <118701318+Jeff-the-simp@users.noreply.github.com>
2023-05-10 09:47:48 +0800

    color
    added button color

---
+++ lesson13.js
@@ -0,0 +1,38 @@
+const song1 = new Audio('funky town low quality.mp3')
+
+function playSongOne() {
+    console.log('playing song 1')
+    song1.play()
+}
+
+function stopSongOne() {
+    console.log('song 1 is stopped')
+    song1.pause()
+}
+
+const song2 = new Audio('Ronnie Coleman Screams.mp3')
+
+function playSongTwo() {
+    console.log('playing song 2')
+    song2.play()
+}
+
+function stopSongTwo() {
+    console.log('song 2 is stopped')
+    song2.pause()
+}
+
+const song3 = new Audio('ONE TWO BUCKLE MY SHOE PHONK.mp3')
+
+function playSongThree() {
+    console.log('playing song 3')
+    song3.play()
+}
+
+function stopSongThree() {
+    console.log('song 3 is stopped')
+    song3.pause()
+}
+play()
+pause()
+load()
